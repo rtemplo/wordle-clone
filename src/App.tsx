0a7a19cbe7e85eda6c470d5c541a84f370481ffb
rtemplo <30515414+rtemplo@users.noreply.github.com>
2026-02-04 11:29:26 -0500

    Init Boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="container">
+      <div className="gameLayer">
+        <div className="title">Wordle Clone</div>
+        <div className="wordGrid">
+          <div className="word">
+            <div className="letter">W</div>
+            <div className="letter">O</div>
+            <div className="letter">R</div>
+            <div className="letter">D</div>
+            <div className="letter">S</div>
+          </div>
+          <div className="word">
+            <div className="letter">G</div>
+            <div className="letter">R</div>
+            <div className="letter">I</div>
+            <div className="letter">D</div>
+            <div className="letter">S</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
